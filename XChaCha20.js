50312801d32a162aa60190252684445cfb47ed43
Ziga <ziga.zajc007@gmail.com>
2022-09-10 09:15:42 +0200

    Replaced Math.random with crypto.getRandomValues

--- XChaCha20.js
+++ XChaCha20.js
@@ -21,19 +21,19 @@ class XChaCha20{
     var hex, i;
 
     let result = [];
-    for (i=0; i<data.length; i++) {
+    for (i = 0; i < data.length; i++) {
       hex = data.charCodeAt(i).toString(16);
       result.push(parseInt(hex,16));
     }
-  
+
     return result
   }
 
   Qround(state, a,b,c,d){
     state[a] += state[b]; state[d] ^= state[a]; state[d] = this.rotateleft(state[d], 16);
     state[c] += state[d]; state[b] ^= state[c]; state[b] = this.rotateleft(state[b], 12);
-    state[a] += state[b]; state[d] ^= state[a]; state[d] = this.rotateleft(state[d], 8); 
-    state[c] += state[d]; state[b] ^= state[c]; state[b] = this.rotateleft(state[b], 7); 
+    state[a] += state[b]; state[d] ^= state[a]; state[d] = this.rotateleft(state[d], 8);
+    state[c] += state[d]; state[b] ^= state[c]; state[b] = this.rotateleft(state[b], 7);
     state[a] >>>= 0;
     state[b] >>>= 0;
     state[c] >>>= 0;
@@ -84,7 +84,7 @@ class XChaCha20{
     let Serialized_Block = []
     for (let i = 0, i2 = 0; i < 16; i++){
       state[i] += temp[i];
-  
+
       Serialized_Block[i2++] = state[i] & 0xFF;
       Serialized_Block[i2++] = (state[i] >>> 8) & 0xFF;
       Serialized_Block[i2++] = (state[i] >>> 16) & 0xFF;
@@ -125,8 +125,8 @@ class XChaCha20{
 
     let B = new Uint32Array(B1.length + B2.length);
     B.set(B1);
-    B.set(B2, B1.length); 
-    
+    B.set(B2, B1.length);
+
     let Serialized_Block = []
     for (let i = 0, i2 = 0; i < 8; i++){
       Serialized_Block[i2++] = B[i] & 0xFF;
@@ -141,18 +141,18 @@ class XChaCha20{
     let keystream = []
     keystream.push(...(this.Chacha20_BlockFunction(key, nonce, counter)));
     let pos = 0;
-    for(let i=0; i<plaintext.length; i++){
+    for(let i = 0; i < plaintext.length; i++){
       if(pos == 64){
         counter++;
-        keystream.push(...(this.Chacha20_BlockFunction(key, nonce, counter)));     
+        keystream.push(...(this.Chacha20_BlockFunction(key, nonce, counter)));
         pos = 0;
       }
       plaintext[i] = parseInt(plaintext[i],16);
       pos++;
     }
-      
+
     let cipherText = []
-    for(let i =0; i<=plaintext.length; i++){
+    for(let i = 0; i <= plaintext.length; i++){
       cipherText[i] = plaintext[i] ^ keystream[i];
     }
     this.keystream = keystream;
@@ -163,17 +163,17 @@ class XChaCha20{
     let keystream = []
     keystream.push(...(this.Chacha20_BlockFunction(key, nonce, counter)));
     let pos = 0;
-    for(let i=0; i<eT.length; i++){
+    for(let i = 0; i < eT.length; i++){
       if(pos == 64){
         counter++;
-        keystream.push(...(this.Chacha20_BlockFunction(key, nonce, counter)));     
+        keystream.push(...(this.Chacha20_BlockFunction(key, nonce, counter)));
         pos = 0;
       }
       pos++;
     }
-      
+
     let pT = []
-    for(let i =0; i<this.encryptedText.length; i++){
+    for(let i = 0; i < this.encryptedText.length; i++){
       pT[i] = this.encryptedText[i] ^ keystream[i];
     }
     this.plaintext = pT
@@ -183,7 +183,7 @@ class XChaCha20{
     this.encryptedText = this.int2(atob(encryptedText));
     let nonce = this.encryptedText.slice(-24);
     this.encryptedText = this.encryptedText.slice(0,-24);
-    
+
     let subkey = this.HChacha20_BlockFunction(key, nonce.slice(0,16));
     let chacha20_nonce = [0x00, 0x00, 0x00, 0x00];
     chacha20_nonce.push(...(nonce.slice(16,24)));
@@ -199,7 +199,7 @@ class XChaCha20{
 
   static convertToText(data){
     let text = '';
-    for(let i=0;i<data.length;i++){
+    for(let i = 0; i < data.length; i++){
       text += String.fromCharCode(data[i]);
     }
     return text;
@@ -209,20 +209,18 @@ class XChaCha20{
     var hex, i;
 
     let result = [];
-    for (i=0; i<data.length; i++) {
+    for (i = 0; i < data.length; i++) {
       hex = data.charCodeAt(i).toString(16);
       result.push(hex);
     }
-  
+
     return result
   }
 
   static randomNonce(){
-    let rand_n = []
-    for(let i=0;i<24;i++){
-      rand_n.push(Math.floor(Math.random() * 255));
-    }
-    return rand_n;
+		var rand_n = new Uint8Array(24);
+    crypto.getRandomValues(rand_n);
+		return rand_n;
   }
 
   static encrypt(message, secretKey){
@@ -244,6 +242,6 @@ class XChaCha20{
     let d1 = new XChaCha20();
     d1.xchacha20_decrypt(secretKey, message);
 
-    return XChaCha20.convertToText(d1.plaintext);
+    return XChaCha20.convertToText(d1.plaintext).replace('\x00','');
   }
 }
