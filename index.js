6bcf6ded48da69b0b62cba59d8183564d5e28643
Ziga <ziga.zajc007@gmail.com>
2022-10-05 16:37:52 +0200

    Changed from spaces to tabs

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
 // Encrypt text
 document.getElementById("btn-encrypt").addEventListener('click', () => {
-  let textPlan = document.getElementById("text-1").value;
-  let secretKey = document.getElementById("secretKey").value;
+	let textPlan = document.getElementById("text-1").value;
+	let secretKey = document.getElementById("secretKey").value;
 
-  document.getElementById("text-2").value = XChaCha20.encrypt(textPlan, secretKey);
+	document.getElementById("text-2").value = XChaCha20.encrypt(textPlan, secretKey);
 });
 
 // Decrypt text
 document.getElementById("btn-decrypt").addEventListener('click', () => {
-  let textEncrypted = document.getElementById("text-1").value;
-  let secretKey = document.getElementById("secretKey").value;
+	let textEncrypted = document.getElementById("text-1").value;
+	let secretKey = document.getElementById("secretKey").value;
 
-  document.getElementById("text-2").value = XChaCha20.decrypt(textEncrypted, secretKey);
+	document.getElementById("text-2").value = XChaCha20.decrypt(textEncrypted, secretKey);
 });
 
 function calcT(timer){
-  return Date.now() - timer;
+	return Date.now() - timer;
 }
 
 // Performance test
 document.getElementById("btn-start").addEventListener("click", () => {
-  let amount = document.getElementById("amount").value;
-  if(amount < 1) amount = 1;
-  if(amount > 100000) amount = 100000;
-  let perf = document.getElementById("perf");
-  let messages = [];
-  let encryptedMessages = [];
-  let secretKey = btoa(XChaCha20.convertToText(XChaCha20.randomNonce())) + btoa(XChaCha20.convertToText(XChaCha20.randomNonce()));
-  let timerStart = Date.now();
+	let amount = document.getElementById("amount").value;
+	if(amount < 1) amount = 1;
+	if(amount > 100000) amount = 100000;
+	let perf = document.getElementById("perf");
+	let messages = [];
+	let encryptedMessages = [];
+	let secretKey = btoa(XChaCha20.convertToText(XChaCha20.randomNonce())) + btoa(XChaCha20.convertToText(XChaCha20.randomNonce()));
+	let timerStart = Date.now();
 
-  perf.innerText = "1. Performance test has started.\n";
+	perf.innerText = "1. Performance test has started.\n";
 
-  let timer = Date.now();
-  for(let i = 0; i < amount; i++){
-    messages[i] = btoa(XChaCha20.convertToText(XChaCha20.randomNonce()));
-  }
-  perf.innerText += "2. " + amount + " random messages generated in " + calcT(timer) + " milliseconds.\n";
+	let timer = Date.now();
+	for(let i = 0; i < amount; i++){
+		messages[i] = btoa(XChaCha20.convertToText(XChaCha20.randomNonce()));
+	}
+	perf.innerText += "2. " + amount + " random messages generated in " + calcT(timer) + " milliseconds.\n";
 
-  timer = Date.now();
-  for(let i = 0; i < amount; i++){
-    encryptedMessages[i] = XChaCha20.encrypt(messages[i], secretKey);
-  }
-  perf.innerText += "3. " + amount + " random messages encrypted in " + calcT(timer) + " milliseconds.\n";
+	timer = Date.now();
+	for(let i = 0; i < amount; i++){
+		encryptedMessages[i] = XChaCha20.encrypt(messages[i], secretKey);
+	}
+	perf.innerText += "3. " + amount + " random messages encrypted in " + calcT(timer) + " milliseconds.\n";
 
-  timer = Date.now();
-  for(let i = 0; i < amount; i++){
-    XChaCha20.decrypt(encryptedMessages[i], secretKey);
-  }
-  perf.innerText += "4. " + amount + " random messages decrypted in " + calcT(timer) + " milliseconds.\n";
+	timer = Date.now();
+	for(let i = 0; i < amount; i++){
+		XChaCha20.decrypt(encryptedMessages[i], secretKey);
+	}
+	perf.innerText += "4. " + amount + " random messages decrypted in " + calcT(timer) + " milliseconds.\n";
 
-  perf.innerText += "5. Performance test has completed in " + calcT(timerStart) + " milliseconds.\n";
+	perf.innerText += "5. Performance test has completed in " + calcT(timerStart) + " milliseconds.\n";
 });
